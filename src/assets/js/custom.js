a4031156455a5c2477381759223aa34f6360b609
Ri4yy <maksimov.1913@list.ru>
2023-10-19 19:20:23 +0300

    newcode

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,19 +1,37 @@
-(function($) {
-    $(function() {
-   
-      $('ul.tabs__caption').on('click', 'li:not(.active-tab)', function() {
-        $(this)
-        .addClass('active-tab').siblings().removeClass('active-tab')
-        .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-   
-      var tabIndex = window.location.hash.replace('#tab','')-1;
-      if (tabIndex != -1) $('ul.tabs__caption li').eq(tabIndex).click();
-   
-      $('a[href*=#tab]').click(function() {
-        var tabIndex = $(this).attr('href').replace(/(.*)#tab/, '')-1;
-        $('ul.tabs__caption li').eq(tabIndex).click();
-      });
-   
+(function ($) {
+    $(function () {
+        $('#openMenu').click(() => {
+            $('#openMenu').toggleClass('after:top-0');
+            $('#openMenu').toggleClass('before:bottom-0');
+            $('#openMenu').toggleClass('after:rotate-45');
+            $('#openMenu').toggleClass('before:-rotate-45');
+            $('#openMenu').find('span').toggleClass('translate-x-full');
+        }); 
+
+        $('.dropdown-btn').click(() => {
+            $('.dropdown-menu').toggleClass('hidden');
+            $('.dropdown-menu').toggleClass('flex');
+
+            $('.dropdown-menu').toggleClass('mt-4');
+            $('.dropdown-btn').toggleClass('after:rotate-180');
+        });
     });
-  })(jQuery);
+
+    $(function () {
+
+        $('ul.tabs__caption').on('click', 'li:not(.active-tab)', function () {
+            $(this)
+                .addClass('active-tab').siblings().removeClass('active-tab')
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+
+        var tabIndex = window.location.hash.replace('#tab', '') - 1;
+        if (tabIndex != -1) $('ul.tabs__caption li').eq(tabIndex).click();
+
+        $('a[href*=#tab]').click(function () {
+            var tabIndex = $(this).attr('href').replace(/(.*)#tab/, '') - 1;
+            $('ul.tabs__caption li').eq(tabIndex).click();
+        });
+
+    });
+})(jQuery);
--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,20 +1,84 @@
-const swiper = new Swiper('.swiper-hero', {
+const swiperHero = new Swiper('.swiper-hero', {
     // Optional parameters
     loop: true,
-  
+
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+        el: '.swiper-pagination',
     },
-  
+
     // Navigation arrows
     navigation: {
-      nextEl: '.main-button-next',
-      prevEl: '.main-button-prev',
+        nextEl: '.main-button-next',
+        prevEl: '.main-button-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+});
+
+const swiperLizing = new Swiper('.swiper-lizing', {
+    spaceBetween: 40,
+    enabled: true,
+
+    breakpoints: {
+      320: {
+        allowTouchMove: true,
+        slidesPerView: 1,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      480: {
+        allowTouchMove: true,
+        spaceBetween: 20,
+        slidesPerView: 3,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      768: {
+        allowTouchMove: true,
+        spaceBetween: 40,
+        slidesPerView: 4,
+        grid: {
+          rows: 1,
+          fill: "row",
+        }
+      },
+      1024: {
+        allowTouchMove: true,
+        slidesPerView: 3,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+      1280: {
+        allowTouchMove: true,
+        slidesPerView: 4,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
+      1921: {
+        allowTouchMove: true,
+        slidesPerView: 5,
+        grid: {
+          rows: 2,
+          fill: "row",
+        }
+      },
     },
-  });
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination-partners',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.main-button-next',
+        prevEl: '.main-button-prev',
+    },
+});
